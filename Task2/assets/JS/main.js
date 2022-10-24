d3c0a4cadf6bb508c69cae9b50af6aaaaa8a6505
ZRavenscroft <110469428+ZRavenscroft@users.noreply.github.com>
2022-10-24 22:36:18 +0400

    Homework

---
+++ Task2/assets/JS/main.js
@@ -0,0 +1,19 @@
+let afruits = document.querySelector('.areafruits');
+let avegetables = document.querySelector('area-vegetables')
+let fruit = document.querySelectorAll('.fruits')
+let vegetable = document.querySelectorAll('.vegetables')
+let dragFruit;
+let dragVegetables;
+
+for (const fruits of fruit) {
+    item.addEventListener('dragstart', ()=>{
+        dragItem = item;
+    })
+}
+for (const areafruits of afruits) {
+    area.addEventListener('dragover',e=>{
+        e.preventDefault();
+    })
+    area.addEventListener('drop',()=>{
+    })
+}
